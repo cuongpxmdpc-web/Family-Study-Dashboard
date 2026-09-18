2476000ae5b12396e5b86b3f893a0705e38ef8be
cuongpxmdpc-web <cuongpx.mdpc@gmail.com>
2026-09-18 11:07:05 +0700

    Prepare Firebase config for V2 Online

---
+++ firebase-config.js
@@ -0,0 +1,4 @@
+// Family Study Dashboard V2 Online
+// Sau khi tạo Firebase Web App, dán firebaseConfig vào object dưới đây.
+// Đây là thông tin cấu hình client (không phải service-account private key).
+window.FAMILY_FIREBASE_CONFIG = null;
